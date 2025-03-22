713b1ad72221e01d9bbbd237e1ec70a394aca30f
陈子进 <wsczj@foxmail.com>
2025-03-23 02:06:28 +0800

    fix: 修复构建的错误12

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-// 添加全局类型声明，避免使用@ts-ignore
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// 创建或复用Prisma客户端实例
 export const prisma =
-  global.prisma ||
+  globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
   });
 
-// 在开发环境中将prisma实例保存到全局对象中以避免热重载时创建多个连接
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
